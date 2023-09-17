1c7a73983132bacc3b3c3aed9487d04454f3f706
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-09-17 22:53:39 +0600

    frameworks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home/Home";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home></Home>,
+    },
+  ]);
   return (
     <div className="App">
-      <h2>Hall Management</h2>
+      <h2 className="text-3xl">Hall Management</h2>
+      <RouterProvider router={router} />
     </div>
   );
 }
