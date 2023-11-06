b8c894f6dd5088af911fee4be6cb3cac89ae03c2
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-11-07 01:21:45 +0600

    hall card

--- src/Pages/Halls/Halls.js
+++ src/Pages/Halls/Halls.js
@@ -1,18 +1,26 @@
 import React, { useEffect, useState } from 'react';
+import Hall from './Hall';
 
 const Halls = () => {
-  const [Hall, setHall] = useState([]);
+  const [Halls, setHalls] = useState([]);
 
   useEffect(() => {
     fetch('data.json')
       .then((res) => res.json())
-      .then((data) => setHall(data));
+      .then((data) => setHalls(data));
   }, []);
-  console.log(Hall);
+  // console.log(Halls);
 
   return (
     <div>
       <h2>All Halls</h2>
+      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center	shadow-xl gap-5'>
+        {Halls.map((hall) => (
+          <Hall
+            hall={hall}
+            key={hall.id}></Hall>
+        ))}
+      </div>
     </div>
   );
 };
