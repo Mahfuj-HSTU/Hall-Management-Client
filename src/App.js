b1b3963f70d76287647ecf335ce5748d18ed5c39
Mahfuj-HSTU <rmahfujur16@gmail.com>
2023-11-09 17:09:54 +0600

    correct hall card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
+import 'alpinejs';
 import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes';
 
 function App() {
   return (
-    <div className='App'>
+    <div className='App text-blue-900	'>
       <RouterProvider router={router} />
     </div>
   );
--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -6,7 +6,9 @@ const Main = () => {
   return (
     <div>
       <Header></Header>
-      <Outlet></Outlet>
+      <div className='max-w-screen-xl mx-auto'>
+        <Outlet></Outlet>
+      </div>
     </div>
   );
 };
