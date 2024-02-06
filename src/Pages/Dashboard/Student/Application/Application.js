7d81dbdf760c549625c60385cd22ffb56e92c204
mahfuj <rmahfujur@gmail.com>
2024-02-06 23:18:49 +0600

    add application prograss

--- src/Pages/Dashboard/Student/Application/Application.js
+++ src/Pages/Dashboard/Student/Application/Application.js
@@ -16,25 +16,34 @@ const Application = () => {
   };
 
   return (
-    // <div>
-    <div className='grid grid-cols-2 place-items-center h-full gap-9 mx-5'>
-      <div className='shadow-xl shadow-slate-300 rounded-xl p-5 w-full'>
-        <h2 className='text-3xl mb-5'>Apply for a Hall Seat</h2>
-        <button
-          onClick={handleHallSeat}
-          className='btn btn-success px-6'>
-          Apply
-        </button>
+    <div className='grid place-items-center h-full mx-5'>
+      <span className='grid grid-cols-2 w-full gap-9'>
+        <div className='shadow-xl shadow-slate-300 rounded-xl p-5'>
+          <h2 className='text-3xl mb-5'>Apply for a Hall Seat</h2>
+          <button
+            onClick={handleHallSeat}
+            className='btn btn-success px-6'>
+            Apply
+          </button>
+        </div>
+        <div className='shadow-xl shadow-slate-300 rounded-xl p-5'>
+          <h2 className='text-3xl mb-5'>Apply for Hall Clearence</h2>
+          <button
+            onClick={handleHallClearence}
+            className='btn btn-success px-6'>
+            Apply
+          </button>
+        </div>
+      </span>
+      <div>
+        <h2 className='text-3xl mb-5'>Application progess</h2>
+        <ul className='steps steps-vertical lg:steps-horizontal'>
+          <li className='step step-primary'>Received</li>
+          <li className='step step-primary'>Checking</li>
+          <li className='step'>Purchase</li>
+          <li className='step'>Receive Product</li>
+        </ul>
       </div>
-      <div className='shadow-xl shadow-slate-300 rounded-xl p-5 w-full'>
-        <h2 className='text-3xl mb-5'>Apply for Hall Clearence</h2>
-        <button
-          onClick={handleHallClearence}
-          className='btn btn-success px-6'>
-          Apply
-        </button>
-      </div>
-      {/* </div> */}
     </div>
   );
 };
