b0d15a6bd8e560469485a3dad527af6ec0a21a38
Hridoy <hunter@hridoy.netis.cc>
2024-02-12 21:36:20 +0600

    removed unwanted text

--- src/Pages/Halls/HallCard.js
+++ src/Pages/Halls/HallCard.js
@@ -8,22 +8,23 @@ const HallCard = ({ hall, isLoading }) => {
   return (
     <div>
       {isLoading ? (
-        <div className='skeleton w-32 h-32'></div>
+        <div className="skeleton w-32 h-32"></div>
       ) : (
         <Link
           to={`/hall/${_id}`}
-          target='blank'
-          className='card w-96 bg-base-100 shadow-xl'>
+          target="blank"
+          className="card w-96 bg-base-100 shadow-xl"
+        >
           <figure>
             <img
-              className='h-52 w-full'
+              className="h-52 w-full"
               src={img || banner}
-              alt='hall images'
+              alt="hall images"
             />{' '}
           </figure>
-          <div className='card-body text-start'>
-            <h2 className='card-title'>{name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
+          <div className="card-body text-start">
+            <h2 className="card-title">{name}</h2>
+            <p></p>
           </div>
         </Link>
       )}
