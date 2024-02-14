b37e17f108cd882ddd5967f6398f2cb1e8109f31
Hridoy <hunter@hridoy.netis.cc>
2024-02-14 10:52:59 +0600

    student dashboard

--- src/Pages/Dashboard/Student/StudentDashboard.js
+++ src/Pages/Dashboard/Student/StudentDashboard.js
@@ -24,15 +24,32 @@ const StudentDashboard = () => {
       fetch(`${ServerLink}/api/students/${us?.sid}`).then((res) => res.json()),
   });
   // console.log(student);
-  const { data: application = {} } = useQuery({
-    queryKey: ['application'],
-    queryFn: () =>
-      fetch(`${ServerLink}/api/applications/${us?.sid}`).then((res) =>
-        res.json()
-      ),
-  });
-  console.log(application);
+  // const { data: applications = [],  } = useQuery({
+  //   queryKey: ['applications'],
+  //   queryFn: () =>
+  //     fetch(`${ServerLink}/api/applications`).then((res) => res.json()),
+  // });
 
+  // const filteredApplications = applications?.filter((user) => {
+  //   refetch();
+  //   return user.hall === details.hallName;
+  // });
+
+  // const { data: application = {} } = useQuery({
+  //   queryKey: ['application'],
+  //   queryFn: () =>
+  //     fetch(`${ServerLink}/api/applications/${us?.sid}`).then((res) =>
+  //       res.json()
+  //     ),
+  // });
+  // const { data: applications = [] } = useQuery({
+  //   queryKey: ['applications'],
+  //   queryFn: () =>
+  //     fetch(`${ServerLink}/api/applications/${us?.sid}`).then((res) =>
+  //       res.json()
+  //     ),
+  // });
+  // console.log(application);
 
   if (isLoading) {
     <Loading />;
@@ -45,26 +62,19 @@ const StudentDashboard = () => {
         <span className="loading loading-spinner text-primary"></span>
       ) : (
         <div className=" flex flex-col text-center md:text-left  mt-2 md:mx-5 bg-gray-100   ">
-          <div className="flex flex-row md:flex-row  gap-5 p-5">
-            <div className="w-full  shadow-inherit rounded-lg bg-white drop-shadow-lg p-3">
-              <h2 className="text-2xl font-semibold">Your Profile</h2>
-              <img
-                className="w-40 h-44 p-2 rounded-lg"
-                src={student.img}
-                alt="user img"
-              />
-              <h1 className="text-3xl font-semibold">Name: {student.name}</h1>
-              <span>ID: {student.sid}</span>
-              <br />
-              <span>
-                <p>Dept: {student.dept} </p>
-              </span>
+          <div className="w-full  shadow-inherit rounded-lg bg-white drop-shadow-lg p-3 text-center items-center justify-center">
+            <div class="avatar">
+              <div class="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+                <img src={student.img} />
+              </div>
             </div>
-            <div className="w-full  shadow-inherit rounded-lg bg-white drop-shadow-lg p-3">
-              <h2 className="text-2xl font-semibold">Applications</h2>
-              <p>Applied for {application.type} </p>
-            </div>
-          </div>
+            <h1 className="text-3xl font-semibold">{student.name}</h1>
+            <span>ID: {student.sid}</span>
+            <br />
+            <span>
+              <p>{student.dept} </p>
+            </span>
+          </div>{' '}
           <div className="flex flex-row md:flex-row  gap-5 p-5">
             <div className="w-full  shadow-inherit rounded-lg bg-white drop-shadow-lg p-3">
               <h2 className="text-2xl font-semibold">Contacts</h2>
@@ -75,6 +85,16 @@ const StudentDashboard = () => {
               <h2 className="text-2xl font-semibold">Address</h2>
               <p>Present address : {student.present}</p>
               <p>Parmanent address : {student.parmanent}</p>
+            </div>
+          </div>
+          <div className="flex flex-row md:flex-row  gap-5 p-5">
+            <div className="w-full  shadow-inherit rounded-lg bg-white drop-shadow-lg p-3">
+              <h2 className="text-2xl font-semibold">Applications</h2>
+              {/* <p>Applied for {application.type} </p> */}
+            </div>{' '}
+            <div className="w-full  shadow-inherit rounded-lg bg-white drop-shadow-lg p-3">
+              <h2 className="text-2xl font-semibold">Notifications</h2>
+              {/* <p>Applied for {application.type} </p> */}
             </div>
           </div>
         </div>
